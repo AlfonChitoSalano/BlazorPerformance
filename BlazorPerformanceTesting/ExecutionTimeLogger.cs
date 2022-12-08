7239fc2a229c6a921ae8985512c83976523db2af
Chito Salano <Alfonchitosalao2@gmail.com>
2022-12-08 10:32:55 +0800

    add project

---
+++ BlazorPerformanceTesting/ExecutionTimeLogger.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace BlazorPerformanceTesting
+{
+    public class ExecutionTimeLogger : IDisposable
+    {
+        private readonly string _methodName;
+        private readonly Stopwatch _stopwatch;
+
+        public ExecutionTimeLogger([CallerMemberName] string methodName = "")
+        {
+            _methodName = methodName;
+            _stopwatch = Stopwatch.StartNew();
+        }
+
+        public void Dispose()
+        {
+            var message = _methodName + "() took " + _stopwatch.ElapsedMilliseconds + " ms.";
+            Debug.WriteLine(message);
+            Console.WriteLine(message);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ BlazorPerformanceTesting/Pages/Index.razor.cs
@@ -0,0 +1,39 @@
+﻿namespace BlazorPerformanceTesting.Pages
+{
+    public partial class Index
+    {
+        private const string TextString =
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.";
+
+        private readonly Random _random = new();
+
+        private static int _counter;
+        private bool _isDraw;
+        private bool _isDrawUsingMarkUp;
+        private string? _markUpElements;
+
+        private void Draw()
+        {
+            _isDraw = true;
+            _isDrawUsingMarkUp = false;
+        }
+
+        private void DrawUsingMarkUpString()
+        {
+            _isDraw = false;
+            _isDrawUsingMarkUp = true;
+            ++_counter;
+
+            using (new ExecutionTimeLogger($"DrawUsingMarkUpString {_counter}"))
+            {
+                for (var i = 0; i < 5000; i++)
+                {
+                    var divSizeInPx = $"{_random.Next(50, 150)}px";
+                    var newElem =
+                        $"<div style=\"height: {divSizeInPx}; width: {divSizeInPx};overflow:hidden; margin:1px;border:1px solid #000;padding:2px;float:left\">{TextString}</div>";
+                    _markUpElements += newElem;
+                }
+            }
+        }
+    }
+}
